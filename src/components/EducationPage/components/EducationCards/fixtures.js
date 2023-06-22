9cbc72f90ee25b8f0deca8395306bdb6f3c5873f
Jared Dye <jareddye@Jareds-MacBook-Pro.local>
2023-06-22 14:15:34 -0700

    resized some elements

---
+++ src/components/EducationPage/components/EducationCards/fixtures.js
@@ -0,0 +1,29 @@
+export const pageText = {
+	book1: {
+		title: "General Information",
+		information: [
+			"Santa Clara University",
+			"BS of Commerce in MIS",
+			"GPA: 3.947",
+		],
+	},
+	book2: {
+		title: "Awards & Scholarships",
+		information: ["Summa Cum Laude", "Ignatian Award", "ISA Award"],
+	},
+	book3: {
+		title: "Honors Groups",
+		information: ["Beta Gamma Sigma", "Upsilon Pi Epsilon", "Leavey Scholars"],
+	},
+	book4: {
+		title: "Relevant Coursework",
+		information: ["TBD", "TBD", "TBD"],
+	},
+	book5: {
+		title: "Internships",
+		information: [
+			"CDK Global - Software Engineer",
+			"Aspenify - Playbook Designer",
+		],
+	},
+};
