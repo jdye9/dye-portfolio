5427eb4a8b08cd9500b61f7605c62155262957a6
Jared Dye <jjtd2000@gmail.com>
2023-06-26 10:18:00 -0700

    edited gap in EducationCard

--- src/components/EducationPage/components/EducationCards/EducationCards.jsx
+++ src/components/EducationPage/components/EducationCards/EducationCards.jsx
@@ -6,11 +6,11 @@ export const EducationCards = ({ selectedBook }) => {
 			<div
 				className={`rounded-[20px] bg-white h-full w-full mx-auto p-5 shadow-md`}
 			>
-				<div className="flex flex-col font-openSans text-white h-full w-full">
+				<div className="flex flex-col font-openSans justify-center text-white h-full w-full gap-4">
 					<div className="font-openSans leading-tight font-extrabold desktopL:text-[70px] desktopM:text-[60px] mobileS:text-[30px] mobileN:text-[35px] mobileL:text-[45px] text-[25px] text-transparent bg-clip-text bg-gradient-to-br from-light-purple to-light-blue">
 						{pageText[selectedBook].title}
 					</div>
-					<div className="flex flex-col justify-around my-auto w-full gap-5 pt-10">
+					<div className="flex flex-col justify-around my-auto w-full gap-3">
 						{pageText[selectedBook].information.map((info) => (
 							<div className="flex font-openSans leading-tight font-extrabold desktopL:text-[45px] desktopM:text-[35px] mobileS:text-[25px] mobileN:text-[30px] mobileL:text-[40px] text-[20px] text-transparent bg-clip-text bg-gradient-to-br from-light-purple to-light-blue">
 								<div className="desktopM:mr-5 mr-3">✏</div>
