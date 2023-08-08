cac807fed635878dc87f0c432b77d12cc0eddd3d
Jared Dye <jjtd2000@gmail.com>
2023-08-07 19:15:52 -0700

    url change

--- src/adapters/services/mongoDb.ts
+++ src/adapters/services/mongoDb.ts
@@ -5,9 +5,12 @@ import {
 } from "./types";
 
 export const getSkills = async (): Promise<GetSkillsResponse> => {
-	const response = await fetch(`https://dye-portfolio-api/skills/`, {
-		method: "GET",
-	});
+	const response = await fetch(
+		`https://dye-portfolio-api.onrender.com/skills/`,
+		{
+			method: "GET",
+		}
+	);
 	if (response.ok) {
 		return response.json();
 	}
@@ -15,9 +18,12 @@ export const getSkills = async (): Promise<GetSkillsResponse> => {
 };
 
 export const getExperiences = async (): Promise<GetExperiencesResponse> => {
-	const response = await fetch(`http://dye-portfolio-api/experiences/`, {
-		method: "GET",
-	});
+	const response = await fetch(
+		`http://dye-portfolio-api.onrender.com/experiences/`,
+		{
+			method: "GET",
+		}
+	);
 	if (response.ok) {
 		return response.json();
 	}
@@ -25,9 +31,12 @@ export const getExperiences = async (): Promise<GetExperiencesResponse> => {
 };
 
 export const getProjects = async (): Promise<GetProjectsResponse> => {
-	const response = await fetch(`http://dye-portfolio-api/projects/`, {
-		method: "GET",
-	});
+	const response = await fetch(
+		`http://dye-portfolio-api.onrender.com/projects/`,
+		{
+			method: "GET",
+		}
+	);
 	if (response.ok) {
 		return response.json();
 	}
