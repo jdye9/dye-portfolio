afd277c8d9940b88ee78cfe0af4fe80a1b7fbf3b
jdye9 <90059967+jdye9@users.noreply.github.com>
2023-08-20 18:08:00 -0700

    Update Socials.tsx

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -14,7 +14,7 @@ export const Socials = () => {
 				<CustomIconButton
 					icon="fab fa-github desktopXL:text-5xl desktopM:text-4xl text-3xl text-white dark:text-[#1A1A40]"
 					link="https://github.com/jdye9"
-					rippleColor="bg-white"
+					rippleColor="bg-white dark:bg-[#1A1A40]"
 					styling="desktopXXL:h-20 desktopL:h-16 h-12 desktopXXL:w-20 desktopL:w-16 w-12 hover:cursor-pointer bg-gradient-to-br from-light-purple to-light-blue dark:shadow-gray-800 shadow-gray-200 hover:shadow-lg hover:shadow-gray-200 dark:hover:outline dark:shadow-none dark:hover:outline-2 dark:hover:outline-white"
 				/>
 				<AnchorLink href="#CONTACT" offset={80}>
@@ -23,7 +23,7 @@ export const Socials = () => {
 						text="contact me"
 						textStyling="desktopXXL:text-2xl desktopXL:text-xl desktopM:text-lg text-sm font-openSans uppercase font-bold text-white dark:text-[#1A1A40]"
 						styling="desktopXXL:h-20 desktopL:h-16 h-12 desktopXXL:w-52 desktopL:w-44 w-40 bg-gradient-to-br from-light-purple to-light-blue shadow-gray-200 hover:shadow-lg hover:shadow-gray-200 dark:hover:outline dark:shadow-none dark:hover:outline-2 dark:hover:outline-white mx-left"
-						rippleColor="bg-white"
+						rippleColor="bg-white dark:bg-[#1A1A40]"
 					/>
 				</AnchorLink>
 			</div>
