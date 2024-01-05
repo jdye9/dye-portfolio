84bd0f3868a5f898193976356176face3fc895da
Jared Dye <jjtd2000@gmail.com>
2024-01-04 19:09:43 -0800

    anchor tag

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -34,23 +34,22 @@ export const Card = ({
 		<div
 			className={`${styling} rounded-lg shadow-md bg-gradient-to-br from-light-purple to-light-blue hover:shadow-lg dark:hover:outline dark:shadow-none dark:hover:outline-2 dark:hover:outline-white group`}
 		>
+			<video
+				className="relative flex items-center mx-auto my-auto rounded-lg shadow-md -top-3 -left-6 group-hover:shadow-lg dark:group-hover:outline dark:shadow-none dark:group-hover:outline-2 dark:group-hover:outline-white"
+				autoPlay
+				loop
+				muted
+			>
+				{loader && (
+					<img
+						src={isDarkMode ? DarkLogo : LightLogo}
+						alt="logo"
+						className="animate-spin h-[25px] w-[25px]"
+					/>
+				)}
+				{!loader && <source src={downloadUrl} type="video/mp4" />}
+			</video>
 			<a href={url} rel="noreferrer" target="_blank">
-				<video
-					className="relative flex items-center mx-auto my-auto rounded-lg shadow-md -top-3 -left-6 group-hover:shadow-lg dark:group-hover:outline dark:shadow-none dark:group-hover:outline-2 dark:group-hover:outline-white"
-					autoPlay
-					loop
-					muted
-				>
-					{loader && (
-						<img
-							src={isDarkMode ? DarkLogo : LightLogo}
-							alt="logo"
-							className="animate-spin h-[25px] w-[25px]"
-						/>
-					)}
-					{!loader && <source src={downloadUrl} type="video/mp4" />}
-				</video>
-
 				<div className="p-5">
 					<h5 className="mb-2 text-sm font-bold tracking-tight text-white mobileS:text-base mobileM:text-xl dark:text-[#1A1A40] desktopM:text-2xl desktopXL:text-3xl desktopXXL:text-4xl font-openSans">
 						{title}
